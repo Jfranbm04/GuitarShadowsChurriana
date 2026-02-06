0fb51ea290292e3bc919f439e9865613e11a3c83
AGRVideogames <bytanquetor19@gmail.com>
2026-02-06 09:57:58 +0100

    CREATE: Map extension and player script

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int vida = 100;
+    private bool inmune = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.CompareTag("Enemy"))
+        {
+            if (!inmune)
+            {
+                vida -= 10;
+                StartCoroutine(ActivarInmunidad());
+            }
+            
+        }
+    }
+    private IEnumerator ActivarInmunidad(){
+   
+        inmune = true;
+        yield return new WaitForSecondsRealtime(2);
+        inmune = false;
+        
+    }
+}
